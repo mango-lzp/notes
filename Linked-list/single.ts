f5fcea960c5eaf1c7113b0e6d3349c1da2054071
“mango.lin” <mango_lzp@163.com>
2021-06-17 10:39:12 +0800

    chore: linked-list single reverse

--- Linked-list/single.ts
+++ Linked-list/single.ts
@@ -3,19 +3,19 @@ interface node {
   next: any
 }
 
-type INode1 = number | string | null
+type INode = number | string | null
 
-class LinkedListNode1 {
+class LinkedListNode {
   value: number | string | null
-  next: LinkedListNode1 | null = null
+  next: LinkedListNode | null = null
   constructor (value: null | string | number) {
     this.value = value
     return this
   }
 }
 
-class LinkedList1{
-  private head = new LinkedListNode1(null)
+class LinkedList{
+  private head = new LinkedListNode(null)
 
   constructor (ll: any[]) {
     this.insert(ll)
@@ -28,7 +28,7 @@ class LinkedList1{
   
   public showList(showHE = false) {
     const list = []
-    let cur: LinkedListNode1 | null = showHE ? this.head : this.head.next
+    let cur: LinkedListNode | null = showHE ? this.head : this.head.next
     while(cur !== null){
       list.push(cur?.value)
       cur = cur?.next ?? null
@@ -36,8 +36,8 @@ class LinkedList1{
     return list
   }
 
-  private _insert (node: INode1) {
-    const _node = new LinkedListNode1(node)
+  private _insert (node: INode) {
+    const _node = new LinkedListNode(node)
     let cur = this.head!
 
     while((cur.next !== null) && (node! >= cur.value!)){
@@ -46,11 +46,11 @@ class LinkedList1{
     [_node.next, cur.next] = [cur.next, _node]
   }
 
-  private _arrInsert (nodeList: INode1[]) {
+  private _arrInsert (nodeList: INode[]) {
     nodeList.forEach(node => this._insert(node))
   }
 
-  public insert (node: INode1[]) {
+  public insert (node: INode[]) {
     if(Array.isArray(node)){
       this._arrInsert(node)
     } else {
@@ -58,10 +58,10 @@ class LinkedList1{
     }
   }
 
-  public find (node: INode1 | LinkedListNode1, preIndex = 0) {
+  public find (node: INode | LinkedListNode, preIndex = 0) {
     const findList = []
-    let cur: LinkedListNode1 | null = this.head
-    let val = this.isNode(node) ? (node as LinkedListNode1).value : node
+    let cur: LinkedListNode | null = this.head
+    let val = this.isNode(node) ? (node as LinkedListNode).value : node
     while(cur !== null && cur.value !== val){
       cur = cur.next
       findList.unshift(cur)
@@ -69,7 +69,7 @@ class LinkedList1{
     return cur?.value === val ? findList[preIndex as 0] : null
   }
 
-  public remove (node: INode1) {
+  public remove (node: INode) {
     const preNode = this.find(node, 1)
     if(preNode === null) return console.log('node cant be found')
     preNode.next = preNode.next!.next
@@ -79,23 +79,26 @@ class LinkedList1{
     // 无节点，无需反转
     if(this.head.next === null) return
 
-    let cur = this.head.next!
+    let cur = this.head.next
+    let pre = this.head
 
-    // while(cur.pre !== null){
-    //   [cur.next, cur.pre] = [cur.pre, cur.next]
-    //   cur = cur.pre as LinkedListNode1
-    // }
-    // if(cur.pre === null){
-    //   cur.pre = this.head
-    // }
-    if(cur.next?.value === 'head'){
-      cur.next = null
+    while(cur !== null){
+      const next = cur.next
+      // 头节点置空
+      cur.next = pre.value ? pre : null
+      pre = cur;
+      cur = next!;
+      // [cur, cur!.next] = [cur.next as LinkedListNode, pre]   //node的解构赋值有问题,行为预期不同上面
     }
+
+    this.head.next = pre
   }
 }
+const ll1 = new LinkedList([1, 2, 3, 5])
+ll1.remove(5)
+ll1.remove(6)
+ll1.reverse()
+console.log(ll1.showList())
 
-const ll11 = new LinkedList1([1, 2, 3, 5])
-ll11.remove(5)
-ll11.remove(6)
-// ll1.reverse()
-console.log(ll11.showList())
+// 模块化使得ts不报标识符重复的错误
+export {}
